6cdf67da79c3f0db63d5dab780d270e2f65eed78
MoyoBongani <C20309081@mytudublin.ie>
2022-12-12 19:37:31 +0000

    files made

---
+++ template/addPage.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+
+<body>
+
+<h1> Add Student </h1>]
+
+
+</body>
+</html>
---
+++ template/deletePage.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+
+<body>
+
+<h1> Delete Student </h1>]
+
+
+</body>
+</html>
